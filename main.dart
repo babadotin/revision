36f7f74d640ba5b3197dcbc864101c738e3d530d
Shubham <shubhamgoyalk567@gmail.com>
2024-07-30 20:35:32 +0530

    [test]

--- main.dart
+++ main.dart
@@ -2,7 +2,6 @@
 // 2. Write a program to find largest number in a list.
 // 3. Write a program to reverse a string.
 
-// 1. किसी संख्या का भाज्य ज्ञात करने के लिए एक प्रोग्राम लिखें। 2. किसी सूची में सबसे बड़ी संख्या ज्ञात करने के लिए एक प्रोग्राम लिखें। 3. किसी स्ट्रिंग को उल्टा करने के लिए एक प्रोग्राम लिखें।
 void main() {
   // find factorial number
   int numb = 5;
