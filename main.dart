c8879d38c68ad169d75eaf8fecbea64156be0eff
Shubham <shubhamgoyalk567@gmail.com>
2024-07-31 11:52:02 +0530

    [test]

--- main.dart
+++ main.dart
@@ -2,29 +2,25 @@
 // 2. Write a program to find largest number in a list.
 // 3. Write a program to reverse a string.
 
-void main() {
-  // find factorial number
-  int numb = 5;
-  print(factorial(numb));
-// find largest number in a list
-  List<int> numbers = [12, 10, 13, 30, 14, 40, 100, 2, 3000, 0, -1];
-  int largest = numbers[0];
-  for (int number in numbers) {
-    if (number > largest) {
-      largest = number;
-    }
-  }
-  print(largest);
-// reversed string
-  String name = 'mahbuhS';
-  print(name.split('').reversed.join());
-}
-
-// find factorial number
 int factorial(int n) {
   if (n <= 1) {
     return 1;
   } else {
     return n * factorial(n - 1);
   }
+}
+
+void main() {
+  int num = 4;
+  print(factorial(num));
+  List<int> numbers = [12, 10, 13, 300, 14, 4000];
+  int largeNum = numbers[0];
+  for (int number in numbers) {
+    if (number > largeNum) {
+      largeNum = number;
+    }
+  }
+  print(largeNum);
+  String name = 'mahbuhS';
+  print(name.split('').reversed.join());
 }
